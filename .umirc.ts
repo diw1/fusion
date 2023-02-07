a98d1e5314ad9e08ba555f50ae64273fabcdbe69
Kevin Wang <gumtree200@gmail.com>
2023-02-08 10:43:31 +1100

    feat(deploy): Add gh-pages deployment

--- .umirc.ts
+++ .umirc.ts
@@ -2,36 +2,36 @@ import { defineConfig } from '@umijs/max';
 
 // @ts-ignore
 export default defineConfig({
-    antd: {},
-    access: {},
-    dva: {},
-    model: {},
-    initialState: {},
-    request: {
-        dataField:'results'
+  antd: {},
+  access: {},
+  dva: {},
+  model: {},
+  initialState: {},
+  request: {
+    dataField: 'results',
+  },
+  layout: {
+    title: 'SOFIHUB',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
     },
-    layout: {
-        title: 'SOFIHUB',
 
+    {
+      name: 'Login',
+      path: '/login',
+      component: './Login',
+      layout: false,
     },
-    routes: [
-        {
-            path: '/',
-            redirect: '/login',
-        },
-
-        {
-            name: 'Login',
-            path: '/login',
-            component: './Login',
-            layout: false,
-        },
-        {
-            name: 'Dashboard',
-            path: '/dashboard',
-            component: './Dashboard',
-        },
-    ],
-    npmClient: 'yarn',
+    {
+      name: 'Dashboard',
+      path: '/dashboard',
+      component: './Dashboard',
+    },
+  ],
+  npmClient: 'yarn',
+  base: '/fusion/',
+  publicPath: '/fusion/',
 });
-
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,44 +1,51 @@
-import { PageContainer } from '@ant-design/pro-components'
-import { connect } from 'umi'
-import styles from './index.less'
-import { Button, Form, Input, Row } from 'antd'
-import Logo from '@/assets/images/logo.svg'
-import {LockOutlined, UserOutlined} from '@ant-design/icons'
-import { Component } from 'react'
+import { PageContainer } from '@ant-design/pro-components';
+import { connect, Link } from 'umi';
+import styles from './index.less';
+import { Button, Form, Input, Row } from 'antd';
+import Logo from '@/assets/images/logo.svg';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Component } from 'react';
 
 @connect(({ global, loading }) => ({
-    global,
-    loading: loading.models.list,
+  loading: loading.models.list,
 }))
 export default class Login extends Component {
-    render() {
-        const { name } = global
+  render() {
+    const handleOk = (values) => {
+      this.props.dispatch({ type: 'login/login', payload: values });
+    };
 
-        const handleOk = (values) => {
-            this.props.dispatch({ type: 'login/login', payload: values })
-        }
+    return (
+      <PageContainer ghost>
+        <div className={styles.form}>
+          <div className={styles.logo}>
+            <img alt="logo" src={Logo} />
+            <span>{name}</span>
+          </div>
+          <Form onFinish={handleOk}>
+            <Form.Item name="username" rules={[{ required: true }]} hasFeedback>
+              <Input prefix={<UserOutlined />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item name="password" rules={[{ required: true }]} hasFeedback>
+              <Input
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </Form.Item>
 
-        return (
-            <PageContainer ghost>
-                <div className={styles.form}>
-                    <div className={styles.logo}>
-                        <img alt="logo" src={Logo} />
-                        <span>{name}</span>
-                    </div>
-                    <Form onFinish={handleOk}>
-                        <Form.Item name="username" rules={[{ required: true }]} hasFeedback>
-                            <Input prefix={<UserOutlined />} placeholder="Username" />
-                        </Form.Item>
-                        <Form.Item name="password" rules={[{ required: true }]} hasFeedback>
-                            <Input prefix={<LockOutlined />} type="password" placeholder="Password" required />
-                        </Form.Item>
-
-                        <Row>
-                            <Button type="primary" htmlType="submit">Sign in</Button>
-                        </Row>
-                    </Form>
-                </div>
-            </PageContainer>
-        )
-    }
+            <Row>
+              <Link to="/dashboard">
+                {' '}
+                <Button type="primary" htmlType="submit">
+                  Sign in
+                </Button>
+              </Link>
+            </Row>
+          </Form>
+        </div>
+      </PageContainer>
+    );
+  }
 }
